d7e9ed4940b212ad0ca59d3ed4df35cdb8bd34ab
Michael Maramba <mpmaramb@calpoly.edu>
2019-11-13 14:06:58 -0800

    query tweets and store sentiment in mongodb

--- db.py
+++ db.py
@@ -1,12 +1,65 @@
 import pymongo
+import tweepy
+import json
+import requests
 from credentials import *
 
+
+# Credentials
+connection_string = get_connection_string()
+consumer_key = get_consumer_key()
+consumer_secret = get_consumer_secret()
+access_token = get_access_token()
+access_token_secret = get_access_token_secret()
+
+# Connect to MongoDB client using credentials and navigate to collection
 connection_string = get_connection_string()
 client = pymongo.MongoClient(connection_string)
 my_db = client['test']
 my_col = my_db['candidates']
 
-my_dict = {'name': 'joe-biden', 'sentiment': 0, 'time': '9-23-19.8:00PM'}
-x = my_col.insert_one(my_dict)
+# Connect to Twitter API
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
 
-print(x.inserted_id)
+api = tweepy.API(auth)
+
+# Idea: Store max_id values for each candidate
+
+candidates = ['joe biden', 'cory booker', 'pete buttigieg', 'tulsi gabbard',
+    'kamala harris', 'amy klobuchar', 'bernie sanders', 'tom steyer', 
+    'elizabeth warren', 'andrew yang']
+
+for candidate in candidates:
+    qry = "{} -filter:retweets".format(candidate)
+    max_id = None
+
+    # get n*100 tweets for the candidate, maximum 180 queries per 15 min=18k tweets
+    for i in range(1):
+        tweets_about_candidate = api.search(qry, count=5, max_id=max_id)
+        for i, tweet in enumerate(tweets_about_candidate):
+            print(i, tweet.text)
+            max_id = tweet.id
+
+        for tweet in tweets_about_candidate:
+            url = 'http://localhost:5000/predict'
+            headers = {'content-type': 'application/json'}
+            my_obj = {"tweet": tweet.text}
+            res = requests.post(url, json=my_obj, headers=headers)
+            
+            decoded = json.loads(res.text)
+            print(decoded['prediction'])
+            print(candidate)
+            print(tweet.created_at)
+            row = {
+                "name": candidate,
+                "time": tweet.created_at,
+                "sentiment": decoded['prediction']
+            }
+            my_col.insert_one(row)
+
+# Get all last 7 days:
+# 1. Query until 18k is reached (n*100)
+# 2. Wait 15 minutes
+# 3. Go back to step 1 until no more tweets are retrieved from qry
+# 4. Repeat every week
