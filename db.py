a5f810e3e947377f1894f7e7965d3686b3f6a984
Michael Maramba <mpmaramb@calpoly.edu>
2019-11-16 16:00:50 -0800

    database changes

--- db.py
+++ db.py
@@ -32,14 +32,25 @@ candidates = ['joe biden', 'cory booker', 'pete buttigieg', 'tulsi gabbard',
 
 for candidate in candidates:
     qry = "{} -filter:retweets".format(candidate)
-    max_id = None
-
+    #max_id = None
+    max_id_cur = my_db['query_ids'].find({"name" : candidate})
+    try:
+        max_id = max_id_cur.next()['last_id']
+    except:
+        max_id = None
+    print("Starting at tweet id:", max_id, "for candidate:", candidate)
+    
+    curr_first_tweet_id = None
+    curr_last_tweet_id = None
     # get n*100 tweets for the candidate, maximum 180 queries per 15 min=18k tweets
     for i in range(1):
-        tweets_about_candidate = api.search(qry, count=5, max_id=max_id)
+        tweets_about_candidate = api.search(qry, count=100, max_id=max_id)
         for i, tweet in enumerate(tweets_about_candidate):
-            print(i, tweet.text)
+            #print(i, tweet.text)
+            if not curr_first_tweet_id:
+                curr_first_tweet_id = tweet.id
             max_id = tweet.id
+        curr_last_tweet_id = max_id
 
         for tweet in tweets_about_candidate:
             url = 'http://localhost:5000/predict'
@@ -48,15 +59,24 @@ for candidate in candidates:
             res = requests.post(url, json=my_obj, headers=headers)
             
             decoded = json.loads(res.text)
-            print(decoded['prediction'])
-            print(candidate)
-            print(tweet.created_at)
+            #print(decoded['prediction'])
+            #print(candidate)
+            #print(tweet.created_at)
             row = {
                 "name": candidate,
                 "time": tweet.created_at,
                 "sentiment": decoded['prediction']
             }
             my_col.insert_one(row)
+    
+
+    id_col = my_db['query_ids']
+    id_row = {
+        "name": candidate,
+        "first_id": curr_first_tweet_id,
+        "last_id": curr_last_tweet_id
+    }
+    id_col.update_one({"name": candidate}, {"$set": {"last_id": curr_last_tweet_id} }, upsert=True)
 
 # Get all last 7 days:
 # 1. Query until 18k is reached (n*100)
