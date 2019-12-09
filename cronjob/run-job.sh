787fbc674588627d87521fdc7a20b91aa5d1a521
Michael Maramba <mpmaramb@calpoly.edu>
2019-12-08 22:00:47 -0800

    demo

--- cronjob/run-job.sh
+++ cronjob/run-job.sh
@@ -1,3 +1,3 @@
-/Library/Frameworks/Python.framework/Versions/3.7/bin/python3 ~/482-cron-job/job.py
+/usr/local/bin/python3 ~/482-cron-job/job.py
 # TODO make python path dynamic
 # $(/usr/bin/which python3) ~/482-cron-job/job.py
